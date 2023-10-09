56a222cc6d895637f3ee7b5652db66a17c8b3911
Malak Bouaker <bouaker.malak@outlook.fr>
2023-10-09 15:07:29 +0200

    tmp

--- src/app/services/aubeSport.service.ts
+++ src/app/services/aubeSport.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/User.model';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AubeSportService {
-  constructor(private http: HttpClient) {}
+  private jwt: string|null; // Déplacez la récupération du JWT dans le constructeur
 
-  users: User[];
+  constructor(private http: HttpClient) {
+    // Récupérez le JWT depuis le localStorage dans le constructeur
+    this.jwt = localStorage.getItem('jwt');
+  }
+
   getAllUsers(): Observable<User[]> {
-    return this.http.get<User[]>('http://localhost:8000/users');
+    // Créez un en-tête HTTP avec le JWT ici avant de faire la requête
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.jwt}`,
+    });
+
+    // Utilisez cet en-tête lors de l'envoi de la requête
+    return this.http.get<User[]>('http://localhost:8000/users', { headers });
   }
 }
