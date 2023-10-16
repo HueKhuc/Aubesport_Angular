1a1af959f2c7c4bef4ddf6d7bf8315ccc005e1af
huekhuc <khuchue.hus@gmail.com>
2023-10-17 00:21:42 +0200

    refactor: user-page component

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { UserService } from '../services/user.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { User } from '../models/User.model';
 
 @Component({
@@ -8,45 +9,79 @@ import { User } from '../models/User.model';
   templateUrl: './user-page.component.html',
   styleUrls: ['./user-page.component.css']
 })
-export class UserPageComponent {
+
+export class UserPageComponent implements OnInit {
 
   user: User;
-  editingField: string = '';
-
-  userFields = ['pseudo', 'bio', 'firstName', 'lastName', 'gender', 'birthday'];
-
-  userFieldNames: { [key: string]: string } = {
-    pseudo: 'Pseudo',
-    bio: 'Bio',
-    firstName: 'First Name',
-    lastName: 'Last Name',
-    gender: 'Gender',
-    birthday: 'Birthday'
-  };
+  profileForm: FormGroup;
+  submitted = false;
+  message: string | null = null;
+  isError: boolean;
 
   constructor(
     private http: HttpClient,
-    private userService: UserService
+    private userService: UserService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit(): void {
     this.userService.getUser().subscribe(response => {
       this.user = response;
     });
-  }
 
-  startEditing(field: string) {
-    this.editingField = field;
-  }
-
-  cancelEditing() {
-    this.editingField = '';
-  }
-
-  saveChanges() {
-    this.userService.updateUser(this.user).subscribe(updatedUser => {
-      console.log('Updated user:', updatedUser);
-      this.editingField = '';
+    this.profileForm = this.fb.group({
+      email: [{ value: '', disabled: true }],
+      pseudo: [''],
+      bio: [''],
+      firstName: [''],
+      lastName: [''],
+      gender: [''],
+      birthday: [{ value: Date }]
     });
+
+    this.userService.getUser().subscribe((user) => {
+      this.user = user;
+      this.profileForm.patchValue({
+        email: user.email,
+        pseudo: user.pseudo,
+        bio: user.bio,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        gender: user.gender,
+        birthday: user.birthday
+      });
+    });
+  }
+
+  onSubmit() {
+    if (this.submitted) {
+      return;
+    }
+    const updatedUser: User = {
+      ...this.user,
+      pseudo: this.profileForm.get('pseudo')?.value ?? false,
+      bio: this.profileForm.get('bio')?.value ?? false,
+      firstName: this.profileForm.get('firstName')?.value ?? false,
+      lastName: this.profileForm.get('lastName')?.value ?? false,
+      gender: this.profileForm.get('gender')?.value ?? false,
+      birthday: this.profileForm.get('birthday')?.value ?? false,
+    };
+
+    this.submitted = true;
+    this.message = null;
+
+    this.userService.updateUser(updatedUser).subscribe(
+      (response) => {
+        console.log('Updated user:', response);
+        this.submitted = false;
+        this.message = 'Profile updated successfully.';
+        this.isError = false;
+      },
+      (error) => {
+        this.submitted = false;
+        this.message = error.error.detail;
+        this.isError = true;
+      }
+    );
   }
 }
