380755200e405e660eb6e6450666d2c09f4c2608
Malak Bouaker <bouaker.malak@outlook.fr>
2023-10-23 13:00:10 +0200

    mise en place du changement de la couleur de la navbar en fonction de la page

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,19 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css'],
 })
-export class NavComponent implements OnInit {
-  isHomePage: boolean = false;
-  constructor(private router: Router) {
-    if(this.isHomePage = this.router.url === 'home'){
-      this.isHomePage = true;
-    }
-    // this.isHomePage = this.router.url === 'home';
-  }
+export class NavComponent  {
 
-  ngOnInit(): void {}
+
+  isHomePage: boolean; 
+
+  constructor(private router: Router) {
+    this.isHomePage = true; 
+
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.isHomePage = (event.url === '/home');
+      }
+    });
+    console.log(this.isHomePage);
+  }
 }
+
+
+  // changeBackground(){
+  //   document.querySelector('.navbar')?.classList.add('navbar-colored');
+  //   document.querySelector('.navbar')?.classList.remove('navbar');
+  //   console.log("le changement de couluer");
+  //   console.log(document.querySelector('.navbar')?.classList);
+  //   }
+    // this.router.navigate('')
+  
+  // linear-gradient(157deg, rgba(1,15,105,0.9275910193178833) 23%, rgba(154,0,120,0.9303921397660627) 77%)
+
